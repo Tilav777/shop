d46f4af8bdb5e16cfba7b3dbca17b451d1d7c21b
tilav <tilav@tilav.tilav>
2023-06-30 09:17:00 +0500

    create module

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,11 @@
 const path = require('path')
 
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].bundle.js',
+    },
+
+    
+}
