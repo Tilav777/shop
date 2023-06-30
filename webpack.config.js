d53aba7dc752d1336af0f9a388e947a0d919cbcd
tilav <tilav@tilav.tilav>
2023-07-01 00:31:27 +0500

    xatolik bartaraf etildi

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,20 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plagin')
-
-module.exports = {
-    mode: 'development', // production
-    entry: path.resolve(__dirname, './src/js/app.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js', //[name].[contenthash].
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Shopping',
-            filename: 'index.html',
-            template: './src/html/tempIndex.html',
-        }),
-        new HtmlWebpackPlugin({
-            title: 'About',
-            filename: 'about.html',
-            template: './src/html/tempAbout.html',
-        }),
-    ],
-
-
-}
+// const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // module.exports = {
-//     mode: 'development',
+//     mode: 'development', // production
 //     entry: path.resolve(__dirname, './src/js/app.js'),
 //     output: {
 //         path: path.resolve(__dirname, 'dist'),
-//         filename: 'bundle.js'
+//         filename: 'bundle.js', //[name].[contenthash].
 //     },
 
 //     module: {
 //         rules: [
 //             {
 //                 test: /\.css$/i,
-//                 use: ["style-loader", "css-loader"]
-//             }
+//                 use: ["style-loader", "css-loader"],
+//             },
 //         ]
 //     },
 
@@ -63,4 +30,40 @@ module.exports = {
 //             template: './src/html/tempAbout.html',
 //         }),
 //     ],
+
+
 // }
+
+
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/js/app.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Shopping',
+            filename: 'index.html',
+            template: './src/html/tempIndex.html',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'About',
+            filename: 'about.html',
+            template: './src/html/tempAbout.html',
+        }),
+    ],
+};
