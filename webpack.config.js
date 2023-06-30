345fbce87e7a2056b8fb33c17ae7132cb11cb356
tilav <tilav@tilav.tilav>
2023-07-01 01:48:21 +0500

    webpack.config.js edited

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,77 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//     mode: 'development',
-//     entry: path.resolve(__dirname, './src/js/app.js'),
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'bundle.js',
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.css$/i,
-//                 use: ['style-loader', 'css-loader'],
-//             },
-//         ],
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             title: 'Shopping',
-//             filename: 'index.html',
-//             template: './src/html/tempIndex.html',
-//         }),
-//         new HtmlWebpackPlugin({
-//             title: 'About',
-//             filename: 'about.html',
-//             template: './src/html/tempAbout.html',
-//         }),
-//     ],
-// };
-
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, './src/js/app.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].[contenthash].bundle.js',
+        clean: true
     },
-
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
-
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Shopping',
             filename: 'index.html',
-            template: './src/html/tempIndex.html'
+            template: './src/html/tempIndex.html',
         }),
         new HtmlWebpackPlugin({
             title: 'About',
             filename: 'about.html',
-            temlate: './src/tempAbout.html'
-        })
-    ]
-}
+            template: './src/html/tempAbout.html',
+        }),
+    ],
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    }
+};
+
+// const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// module.exports = {
+//     mode: 'development',
+//     entry: path.resolve(__dirname, './src/js/app.js'),
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'bundle.js'
+//     },
+
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/i,
+//                 use: ["style-loader", "css-loader"]
+//             }
+//         ]
+//     },
+
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             title: 'Shopping',
+//             filename: 'index.html',
+//             template: './src/html/tempIndex.html'
+//         }),
+//         new HtmlWebpackPlugin({
+//             title: 'About',
+//             filename: 'about.html',
+//             temlate: './src/tempAbout.html'
+//         })
+//     ]
+// }
